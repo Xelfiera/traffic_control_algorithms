f938130ea57c3741deec3aa3a9e999d7f5ac32a9
Xelfiera <119340705+Xelfiera@users.noreply.github.com>
2023-10-27 10:27:09 +0300

    .xlsx changed, fuzzyST design completed, webster cleared

--- 01_FuzzyLogic/fuzzyST_design.py
+++ 01_FuzzyLogic/fuzzyST_design.py
@@ -5,7 +5,7 @@ from skfuzzy import control as ctrl
 no_waiting_vehicles = ctrl.Antecedent(np.arange(0, 13, 1), 'no_waiting_vehicles')
 no_lanes_on_edge = ctrl.Antecedent(np.arange(0, 5, 0.5), 'no_lanes_on_edges')
 
-signal_time = ctrl.Consequent(np.arange(0, ))
+signal_time = ctrl.Consequent(np.arange(0, 51, 1), 'signal_time')
 
 no_waiting_vehicles['less'] = fuzzy.zmf(no_waiting_vehicles.universe, 2, 4)
 no_waiting_vehicles['medium'] = fuzzy.trimf(no_waiting_vehicles.universe, [3, 5, 7])
@@ -18,3 +18,47 @@ no_lanes_on_edge['two'] = fuzzy.trimf(no_lanes_on_edge.universe, [1.5, 2, 2.5])
 no_lanes_on_edge['three'] = fuzzy.trimf(no_lanes_on_edge.universe, [2.5, 3, 3.5])
 no_lanes_on_edge['four'] = fuzzy.smf(no_lanes_on_edge.universe, 3.5, 4)
 # no_lanes_on_edge.view()
+
+signal_time['short'] = fuzzy.zmf(signal_time.universe, 5, 10)
+signal_time['medium'] = fuzzy.trimf(signal_time.universe, [7, 15, 25])
+signal_time['long'] = fuzzy.trimf(signal_time.universe, [20, 27, 35])
+signal_time['too-long'] = fuzzy.smf(signal_time.universe, 30, 40)
+# signal_time.view()
+
+rule_a0 = ctrl.Rule(no_waiting_vehicles['less'] & no_lanes_on_edge['one'], signal_time['medium'])
+rule_b0 = ctrl.Rule(no_waiting_vehicles['medium'] & no_lanes_on_edge['one'], signal_time['long'])
+rule_c0 = ctrl.Rule(no_waiting_vehicles['high'] & no_lanes_on_edge['one'], signal_time['long'])
+rule_d0 = ctrl.Rule(no_waiting_vehicles['too-high'] & no_lanes_on_edge['one'], signal_time['too-long'])
+
+rule_a1 = ctrl.Rule(no_waiting_vehicles['less'] & no_lanes_on_edge['two'], signal_time['medium'])
+rule_b1 = ctrl.Rule(no_waiting_vehicles['medium'] & no_lanes_on_edge['two'], signal_time['medium'])
+rule_c1 = ctrl.Rule(no_waiting_vehicles['high'] & no_lanes_on_edge['two'], signal_time['long'])
+rule_d1 = ctrl.Rule(no_waiting_vehicles['too-high'] & no_lanes_on_edge['two'], signal_time['long'])
+
+rule_a2 = ctrl.Rule(no_waiting_vehicles['less'] & no_lanes_on_edge['three'], signal_time['short'])
+rule_b2 = ctrl.Rule(no_waiting_vehicles['medium'] & no_lanes_on_edge['three'], signal_time['medium'])
+rule_c2 = ctrl.Rule(no_waiting_vehicles['high'] & no_lanes_on_edge['three'], signal_time['medium'])
+rule_d2 = ctrl.Rule(no_waiting_vehicles['too-high'] & no_lanes_on_edge['three'], signal_time['long'])
+
+rule_a3 = ctrl.Rule(no_waiting_vehicles['less'] & no_lanes_on_edge['four'], signal_time['short'])
+rule_b3 = ctrl.Rule(no_waiting_vehicles['medium'] & no_lanes_on_edge['four'], signal_time['short'])
+rule_c3 = ctrl.Rule(no_waiting_vehicles['high'] & no_lanes_on_edge['four'], signal_time['medium'])
+rule_d3 = ctrl.Rule(no_waiting_vehicles['too-high'] & no_lanes_on_edge['four'], signal_time['medium'])
+
+signal_time_ctrl = ctrl.ControlSystem([rule_a0, rule_b0, rule_c0, rule_d0,
+                                       rule_a1, rule_b1, rule_c1, rule_d1,
+                                       rule_a2, rule_b2, rule_c2, rule_d2,
+                                       rule_a3, rule_b3, rule_c3, rule_d3,])
+signal_time_status = ctrl.ControlSystemSimulation(signal_time_ctrl)
+
+def calc_signal_time(edge_id, waiting_vehicles, lanes_on_edge):
+    signal_time_status.input['no_waiting_vehicles'] = waiting_vehicles
+    signal_time_status.input['no_lanes_on_edge'] = lanes_on_edge
+    signal_time_status.compute()
+    signal_time_output = signal_time_status.output['priority']
+
+    print(f"Number of waiting vehicles on {edge_id}: {waiting_vehicles}")
+    print(f"Number of lanes on {edge_id}: {lanes_on_edge}")
+    print(f"Signal time of {edge_id}: {signal_time_output}")
+
+    return signal_time_output
